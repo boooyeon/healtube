44a4c9b2bea69626bf877636e8f8e6ee493fa650
boooyeon@gmail.com <boooyeon@gmail.com>
2019-12-05 16:56:31 +0900

    requirement

--- healtube/settings.py
+++ healtube/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'import_export',
+    
 
 
 
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-ko'
 
 TIME_ZONE = 'UTC'
 
--- main/admin.py
+++ main/admin.py
@@ -5,6 +5,9 @@ from .models import Channel
 
 # Register your models here.
 
-admin.site.register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('channel_name', 'health_type', 'health_part', 'video_title', 'video_link', 'video_view_num', 'video_upload_date')
 
+
+admin.site.register(Channel, ChannelAdmin)
 
