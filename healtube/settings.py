0ce109f1148034c8918326442b80acd50c3929ff
boooyeon@gmail.com <boooyeon@gmail.com>
2019-12-10 11:31:35 +0900

    시크릿키 수정

--- healtube/settings.py
+++ healtube/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'ptkbnjq7=95%^aw4fguf+z-qyx5!)g5_aw4ns_bzcwk_r)#9#%'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('ptkbnjq7=95%^aw4fguf+z-qyx5!)g5_aw4ns_bzcwk_r)#9#%', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
